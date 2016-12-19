4188fa4949afdd9590d3bb18a2d70429d5176da3
stender1995 <mstender.1995@gmail.com>
2016-12-19 14:42:41 +0100

    heute

--- Rails/Projekte/rails_app/app/models/event.rb
+++ Rails/Projekte/rails_app/app/models/event.rb
@@ -1,5 +1,11 @@
 class Event < ApplicationRecord
 
+	has_many :registrations, dependent: :destroy
+
+	validates :name, presence: true
+	validates :description, length: {minimum: 25}
+	validates :price, numericality: {greater_than_or_equal_to: 0}
+
 	def free?
 		price.blank? || price.zero?
 	end
